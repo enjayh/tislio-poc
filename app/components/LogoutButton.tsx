6dd157b7e89fc53d4e987a292cbbd8280a5d3e80
enjayh <enjayh@gmail.com>
2023-09-24 15:38:56 -0600

    T28: Fix home button state bug (#29)

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -11,6 +11,7 @@ export default function LogoutButton() {
     const { error } = await supabase.auth.signOut()
 
     if (!error) {
+      router.refresh()
       router.push('/login')
     }
     if (error) {
