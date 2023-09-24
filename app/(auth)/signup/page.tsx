6a980308d6d5633ba33ffecd657e06f4da3dc0e7
En-jay Hsu <enjayh@gmail.com>
2023-09-24 10:38:09 -0600

    T21: Add field to confirm password on sign up

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -8,12 +8,20 @@ import Link from 'next/link'
 export default function SignUp() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [repassword, setRepassword] = useState('')
   const [error, setError] = useState('')
 
   const router = useRouter()
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
+    if (password !== repassword) {
+      setError('Passwords do not match')
+      return
+    }
+
+    setError('')
 
     const supabase = createClientComponentClient()
     const { error } = await supabase.auth.signUp({
@@ -36,29 +44,32 @@ export default function SignUp() {
     <main>
       <h2 className="text-center">Sign up</h2>
       <form onSubmit={handleSubmit}>
-        <label>
-          <span>Email:</span>
-          <input
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            required
-          />
-        </label>
-        <label>
-          <span>Password:</span>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            required
-          />
-        </label>
+        <span>Email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+        <span>Retype Password:</span>
+        <input
+          type="password"
+          onChange={(e) => setRepassword(e.target.value)}
+          value={repassword}
+          required
+        />
         <button className="btn-primary">Submit</button>
       </form>
       <div className="text-center"><Link href="/login">Already have an account? Log in here.</Link></div>
       {error && (
-        <div>{error}</div>
+        <div className="error">{error}</div>
       )}
     </main>
   )
