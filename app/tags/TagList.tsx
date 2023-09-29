a2045aa3dd044ae2dd5d5accf741b54d519eceaa
toddcmz <toddcmz@gmail.com>
2023-09-28 19:50:10 -0600

    tweaking interface for tag

--- app/tags/TagList.tsx
+++ app/tags/TagList.tsx
@@ -2,6 +2,11 @@ import { headers } from "next/headers"
 import { TiDelete } from 'react-icons/ti'
 export const dynamic = 'force-dynamic'
 
+interface Tag {
+  name: string;
+  id: number;
+}
+
 export default async function TagList() {
 
   const res = await fetch('http://localhost:3000/api/tags', {
@@ -17,7 +22,7 @@ export default async function TagList() {
 
   return (
     <>
-      {tags.map((tag) => (
+      {tags.map((tag:Tag) => (
         <div key={tag.id} className="tagStyle">
           <p>{tag.name}</p>
         </div>
