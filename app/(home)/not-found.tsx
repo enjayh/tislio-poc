258093b46c81ee1b07bcfcb9ce34612e543ab28f
En-jay Hsu <enjayh@gmail.com>
2023-10-03 17:32:35 -0600

    Update 'Not found' page

--- app/(home)/not-found.tsx
+++ app/(home)/not-found.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link'
+import SimpleNav from '../components/SimpleNav'
 
 export default function NotFound() {
   return (
     <main className="text-center">
-      <h2 className="text-3xl">Problem!</h2>
-      <p>Can&apos;t find the page you were looking for.</p>
-      <p>Go back to <Link href="/dashboard">Home</Link></p>
+      <SimpleNav />
+      <main>
+        <h2 className="text-3xl">Problem!</h2>
+        <p>Can&apos;t find the page you were looking for.</p>
+        <p>Go back to <Link href="/dashboard">Dashboard</Link></p>
+      </main>
     </main>
   )
 }
